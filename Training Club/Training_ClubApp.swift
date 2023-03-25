efcaf00ed9b07af3f05f544e61ee9cc48d8cee8b
github-personal <filipe.boeck@hotmail.com>
2023-03-25 11:50:18 -0300

    Initial Commit

---
+++ Training Club/Training_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Training_ClubApp.swift
+//  Training Club
+//
+//  Created by wolf on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Training_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
