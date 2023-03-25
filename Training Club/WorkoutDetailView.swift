e47ab7291b5b7a9f029823013484ad5acd69fbcd
github-personal <filipe.boeck@hotmail.com>
2023-03-25 11:56:40 -0300

    feat: first commit

---
+++ Training Club/WorkoutDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  WorkoutDetailView.swift
+//  Training Club
+//
+//  Created by wolf on 25/03/23.
+//
+
+import SwiftUI
+
+struct WorkoutDetailView: View {
+    var body: some View {
+      ScrollView {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      }
+    }
+}
+
+struct WorkoutDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutDetailView()
+    }
+}
